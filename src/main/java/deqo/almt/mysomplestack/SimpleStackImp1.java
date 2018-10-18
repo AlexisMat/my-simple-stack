c8111dfda48516d450eccd0543a9c3f1b0b5b43c
AlexisMat <mateo.alexis82@gmail.com>
2018-10-18 19:35:36 +0200

    Commit des classes pour stack

---
+++ src/main/java/deqo/almt/mysomplestack/SimpleStackImp1.java
@@ -0,0 +1,33 @@
+package deqo.almt.mysomplestack;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class SimpleStackImp1 implements SimpleStack {
+
+    Stack<Object> wrappedStack = new Stack<Object>();
+    @Override
+    public boolean isEmpty() {
+        return wrappedStack.isEmpty();
+    }
+
+    @Override
+    public int getSize() {
+        return wrappedStack.size();
+    }
+
+    @Override
+    public void push(Item item) {
+        wrappedStack.push(item);
+    }
+
+    @Override
+    public Item peek() throws EmptyStackException {
+        return (Item) wrappedStack.peek();
+    }
+
+    @Override
+    public Item pop() throws EmptyStackException {
+        return (Item) wrappedStack.pop();
+    }
+}
